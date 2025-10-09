56d2eefd691dbbe7367d8f39a7487151bd288f88
cj.catalan07 <cj.catalan07@gmail.com>
2025-10-09 20:02:54 +0000

    preact island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://cjdc007-blog.netlify.app/"
+  site: "https://cjdc007-blog.netlify.app/",
+  integrations: [preact()]
 });
